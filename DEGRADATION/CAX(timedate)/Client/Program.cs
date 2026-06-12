d0be45157a18764ff0eeb44274d856de3303d6de
Autumn May Valley <draqoshaimposter@gmail.com>
2026-06-12 13:37:07 +0300

    Add files via upload

---
+++ DEGRADATION/CAX(timedate)/Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TimeClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 1010;
+
+            Socket client = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            try
+            {
+                client.Connect(new IPEndPoint(ip, port));
+
+                Console.WriteLine("1 - Дата");
+                Console.WriteLine("2 - Час");
+
+                string request = Console.ReadLine() == "1"
+                    ? "date"
+                    : "time";
+
+                client.Send(Encoding.UTF8.GetBytes(request));
+
+                byte[] buffer = new byte[1024];
+
+                int bytesReceived = client.Receive(buffer);
+
+                string response =
+                    Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+
+                Console.WriteLine("Відповідь сервера:");
+                Console.WriteLine(response);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
---
+++ DEGRADATION/CAX(timedate)/Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TimeServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 1010;
+
+            Socket server = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            server.Bind(new IPEndPoint(ip, port));
+            server.Listen(10);
+
+            Console.WriteLine("Server running...");
+
+            while (true)
+            {
+                Socket client = server.Accept();
+
+                byte[] buffer = new byte[1024];
+
+                int bytesReceived = client.Receive(buffer);
+
+                string request =
+                    Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+
+                string response;
+
+                if (request.ToLower() == "time")
+                {
+                    response = DateTime.Now.ToLongTimeString();
+                }
+                else if (request.ToLower() == "date")
+                {
+                    response = DateTime.Now.ToShortDateString();
+                }
+                else
+                {
+                    response = "Невідома команда";
+                }
+
+                client.Send(Encoding.UTF8.GetBytes(response));
+
+                client.Close();
+            }
+        }
+    }
+}
